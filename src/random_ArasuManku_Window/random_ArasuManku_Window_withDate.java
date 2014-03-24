434209f8d909edfee7dcc80ab8a8c51b345867dc
robertchristensen <robert.christensen@gmail.com>
2014-03-24 12:33:25 -0600

    +Added a simple implmementation of the sliding window which *should* support querying over dates.  The interface has changed a bit, so I hope it words.

---
+++ src/random_ArasuManku_Window/random_ArasuManku_Window_withDate.java
@@ -0,0 +1,62 @@
+/**
+ * 
+ */
+package random_ArasuManku_Window;
+
+import java.util.Calendar;
+import java.util.Date;
+
+/**
+ * @author robertc
+ *
+ */
+public class random_ArasuManku_Window_withDate extends random_ArasuManku_Window {
+
+	Date_Index_Table m_dateTable;
+	
+	Calendar m_nextTimeRecord;
+	Date     m_nextTimeRecordDate;
+	
+	int m_updateRate;
+	
+	/**
+	 * @param W
+	 * @param epsilon
+	 * @param delta
+	 */
+	public random_ArasuManku_Window_withDate(int W, double epsilon, double delta, int seconds_per_update) {
+		super(W, epsilon, delta);
+		m_dateTable = new Date_Index_Table(10000);
+		m_updateRate = seconds_per_update;
+	}
+	
+	public void insertWDate(String item, Date timeStamp)
+	{
+		if(m_nextTimeRecord == null)
+		{
+			m_nextTimeRecord = Calendar.getInstance();
+			m_nextTimeRecord.add(Calendar.SECOND, m_updateRate);
+			m_nextTimeRecord.setTime(timeStamp);
+			m_dateTable.insertElement(timeStamp, this.get_insertedElements());
+		}
+		else if(timeStamp.after(m_nextTimeRecord.getTime()))
+		{
+			// do a while loop in case we need to update multiple times.  This should
+			// not have to loop many times
+			while(timeStamp.after(m_nextTimeRecord.getTime()))
+				m_nextTimeRecord.add(Calendar.SECOND, m_updateRate);
+			
+			m_dateTable.insertElement(m_nextTimeRecord.getTime(), this.get_insertedElements());
+		}
+		
+		this.insert(item);
+	}
+	
+	public int query(String item, Date startDate, Date endDate)
+	{
+		int lower = m_dateTable.find_lower(startDate);
+		int upper = m_dateTable.find_upper(endDate);
+		
+		return this.query(item, lower, upper);
+	}
+}
